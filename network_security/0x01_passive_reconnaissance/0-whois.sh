249c7712a5b6b49af29bf9998475e482aa8f12e0
alexf027 <alexf027@kali>
2025-04-10 15:55:15 -0500

    Whois

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk -F':' '/(Registrant|Admin|Tech) (Organization|State\/Province|Country|Email)/ {sub(/^ +/, "", $2); print $1 "," $2}' > "$1.csv"}
+whois $1 | awk -F':' '/(Registrant|Admin|Tech) / {gsub(/^ +| +$/, " $2); print $1 "," $2}' > $1.csv
